d0ec44874ea4f9aafd649d3f699afbbec6265368
MnrMine <mnr_mine_agri@hotmail.com>
2024-08-01 09:23:40 +0300

    WebUI

---
+++ Services/Comment/TakeAway.Commnet/Mapping/GeneralMapping.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using TakeAway.Comment.DAl.Entities;
+using TakeAway.Comment.Dtos;
+
+namespace TakeAway.Comment.Mapping
+{
+    public class GeneralMapping : Profile
+    {
+        public GeneralMapping()
+        {
+            CreateMap<UserComment, ResultUserCommentDto>().ReverseMap();
+            CreateMap<UserComment, GetByIdUserCommentDto>().ReverseMap();
+            CreateMap<UserComment, CreateUserCommentDto>().ReverseMap();
+            CreateMap<UserComment, UpdateUserCommentDto>().ReverseMap();
+        }
+    }
+}
